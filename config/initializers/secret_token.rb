6e7a99aecd31d3e991029db22dcb50267ad3ea7d
teebii <eric.hercog@gmail.com>
2013-05-15 12:38:02 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetspace::Application.config.secret_token = '4c622fee5df298fc197e0b869e7846ebc33eb0f04eaaa5bd36f76146d3ce9705572ccbc36da6a491a3a95ef2e8658f048af02c1f47eec993db4b5498ae615d9b'
