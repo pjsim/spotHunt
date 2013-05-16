955ca82256c64c3511f0c9952222b2fca0417bcd
pjsim <phillip.j.simmonds@gmail.com>
2013-05-16 17:42:06 +1000

    search bar mostly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,21 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @venues = Venue.all
+    if params[:search].present?
+      @search_location = VenueLocation.create(:address => params[:search])
+      @venue_locations = VenueLocation.near(params[:search], params[:radius], :order => :distance)
+      @json = VenueLocation.near(params[:search], params[:radius], :order => :distance).to_gmaps4rails
+    else
+      @venue_locations = VenueLocation.all
+      @json = VenueLocation.all.to_gmaps4rails
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @venue_locations }
+      if params[:search].present?
+        @search_location.destroy
+      end
+    end
   end
 end
